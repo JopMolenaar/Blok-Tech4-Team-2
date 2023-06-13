5be487c5f43b767ecf24aa61d1580097bf8ebdf1
Thije Wind <thije.wind@hva.nl>
2023-06-13 18:43:57 +0200

    TEST

--- static/js/confirm-date.js
+++ static/js/confirm-date.js
@@ -9,15 +9,3 @@ tomorrow.setDate(tomorrow.getDate() + 1)
 const minDate = tomorrow.toISOString().split("T")[0]
 getDateInput.min = minDate
 getDateInput.value = minDate
-
-// Calculate maxDate as the last weekday of the year
-const maxDate = new Date(day.getFullYear(), 11, 31) // Set to last day of the current year
-
-while (maxDate.getDay() === 0 || maxDate.getDay() === 6) {
-    maxDate.setDate(maxDate.getDate() - 1)
-}
-
-const maxDateStr = maxDate.toISOString().split("T")[0]
-getDateInput.max = maxDateStr
-
-// This does'nt work :C
