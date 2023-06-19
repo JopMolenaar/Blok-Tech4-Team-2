d12b895cdbf4a849a2fe9b00ed65ddc54396a853
Jop <jopmolenaar@icloud.com>
2023-06-19 16:33:58 +0200

    styling done

---
+++ static/js/checkForAdmin.js
@@ -0,0 +1,11 @@
+//this function checkes the already active options for the product when an admin wants to change something.
+
+const adminAanpassen = document.querySelector("#admin-aanpassen")
+const allPs = adminAanpassen.querySelectorAll("section p.visually-hidden")
+
+allPs.forEach((p) => {
+    console.log(p.textContent)
+    p = p.textContent.replace("It is ", "").toLowerCase()
+    const checkThisOne = adminAanpassen.querySelector(`fieldset input[value=${p}]`)
+    if (checkThisOne) checkThisOne.checked = true
+})
