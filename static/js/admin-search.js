f1aecdb73ff7a14fa7286e1fe8991cade2af22ed
thijew <thijewind@hotmail.com>
2023-06-21 22:56:49 +0200

    WIP css admin page

--- static/js/admin-search.js
+++ static/js/admin-search.js
@@ -1,15 +1,15 @@
-function search() {
-    var input = document.querySelector("#searchAnimals").value.toLowerCase()
-    var animals = document.querySelectorAll("#animalsList ul")
+const search = () => {
+    const input = document.querySelector("#searchAnimals").value.toLowerCase()
+    const animals = document.querySelectorAll("#animalsList ul")
 
-    for (var i = 0; i < animals.length; i++) {
-        var name = animals[i].getElementsByTagName("li")[1].textContent.toLowerCase()
+    animals.forEach((animal) => {
+        const name = animal.getElementsByTagName("li")[1].textContent.toLowerCase()
         if (name.indexOf(input) !== -1) {
-            animals[i].style.display = "block"
+            animal.style.display = "block"
         } else {
-            animals[i].style.display = "none"
+            animal.style.display = "none"
         }
-    }
+    })
 }
 
 document.getElementById("searchAnimals").addEventListener("input", search)
