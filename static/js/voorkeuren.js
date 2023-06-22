955c274e39d43a43bd87200e6e0ab3f86b8603eb
Jop <jopmolenaar@icloud.com>
2023-06-22 15:25:29 +0200

    gulp js uitgecommend

--- static/js/voorkeuren.js
+++ static/js/voorkeuren.js
@@ -8,38 +8,38 @@ const dropdowns = document.querySelectorAll("select")
 let allDropdownsSelected = false // Variabele om bij te houden of alle dropdowns zijn geselecteerd
 
 const updateButton = () => {
-	allDropdownsSelected = true
+    allDropdownsSelected = true
 
-	dropdowns.forEach((dropdown) => {
-		if (dropdown.value === "--") {
-			allDropdownsSelected = false
-		}
-	})
+    dropdowns.forEach((dropdown) => {
+        if (dropdown.value === "--") {
+            allDropdownsSelected = false
+        }
+    })
 
-	nextButton.classList.toggle("selected", allDropdownsSelected)
+    nextButton.classList.toggle("selected", allDropdownsSelected)
 }
 
 dropdowns.forEach((dropdown) => {
-	dropdown.addEventListener("change", updateButton) // Luistert naar wijzigingen in de dropdowns en roept de updateButton-functie aan
+    dropdown.addEventListener("change", updateButton) // Luistert naar wijzigingen in de dropdowns en roept de updateButton-functie aan
 })
 
 nextButton.addEventListener("click", (event) => {
-	if (!allDropdownsSelected) {
-		event.preventDefault()
-		alert("Niet alle onderdelen zijn geselecteerd.")
-	}
+    if (!allDropdownsSelected) {
+        event.preventDefault()
+        alert("Niet alle onderdelen zijn geselecteerd.")
+    }
 })
 
 const updateText = () => {
-	const styleText1 = document.getElementById("styleText1")
-	const styleText2 = document.getElementById("styleText2")
-	const styleText3 = document.getElementById("styleText3")
-	const styleText4 = document.getElementById("styleText4")
+    const styleText1 = document.getElementById("styleText1")
+    const styleText2 = document.getElementById("styleText2")
+    const styleText3 = document.getElementById("styleText3")
+    const styleText4 = document.getElementById("styleText4")
 
-	styleText1.innerHTML = energielevel.value !== "--" ? "checked" : ""
-	styleText2.innerHTML = leefstijl.value !== "--" ? "checked" : ""
-	styleText3.innerHTML = grootte.value !== "--" ? "checked" : ""
-	styleText4.innerHTML = slaapritme.value !== "--" ? "checked" : ""
+    styleText1.innerHTML = energielevel.value !== "--" ? "checked" : ""
+    styleText2.innerHTML = leefstijl.value !== "--" ? "checked" : ""
+    styleText3.innerHTML = grootte.value !== "--" ? "checked" : ""
+    styleText4.innerHTML = slaapritme.value !== "--" ? "checked" : ""
 }
 
 energielevel.addEventListener("change", updateText) // Luistert naar wijzigingen in energielevel en roept de updateText-functie aan
@@ -49,6 +49,6 @@ slaapritme.addEventListener("change", updateText)
 
 // Controleer of JavaScript is ingeschakeld
 document.addEventListener("DOMContentLoaded", () => {
-	const enhancement = document.getElementById("enhancement")
-	enhancement.style.display = "none"
+    const enhancement = document.getElementById("enhancement")
+    enhancement.style.display = "none"
 })
