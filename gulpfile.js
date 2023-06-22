264944b6e49d35ceaedfbef09bcbef17efc99432
Quinesha <qtiemations@gmail.com>
2023-06-22 03:40:36 +0200

    turned functions into arrow functions voorkeuren

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,17 @@ const cleanCSS = require("gulp-clean-css")
 const livereload = require("gulp-livereload")
 
 gulp.task("sass", function () {
-    return gulp
-        .src("./static/styles/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./static/styles/css"))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS())
-        .pipe(livereload())
+	return gulp
+		.src("./static/styles/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest("./static/styles/css"))
+		.pipe(autoprefixer())
+		.pipe(cleanCSS())
+		.pipe(livereload())
 })
 gulp.task("watch", function () {
-    livereload.listen() // Start livereload
-    gulp.watch("./static/styles/*.scss", gulp.series("sass"))
+	livereload.listen() // Start livereload
+	gulp.watch("./static/styles/*.scss", gulp.series("sass"))
 })
 // Default Task
 gulp.task("default", gulp.parallel("watch", "sass"))
